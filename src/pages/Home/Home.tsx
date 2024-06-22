f06a76b09e3b739890abb4b401d86b91d582d8e6
EvgeniiSim <evgenijsimanin74@gmail.com>
2024-06-22 12:27:43 +0500

    Added bg elements to the home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,6 +5,7 @@ import Liga from "./modules/Liga";
 
 import styles from "./Home.module.scss";
 import Energy from "./modules/Energy";
+import Menu from "./modules/Menu";
 const cn = classNames.bind(styles);
 
 const Home = () => {
@@ -16,7 +17,36 @@ const Home = () => {
          </div>
          <div className={cn("bottom")}>
             <Energy total={1000} current={300} />
+            <Menu />
          </div>
+
+         {/* Облака сверху */}
+         {/* <img
+            src="img/pages/home/clouds.svg"
+            className={cn("clouds")}
+            alt="farm"
+         /> */}
+
+         {/* Элементы заднего фона */}
+         <img
+            src="img/pages/home/home-bg.svg"
+            className={cn("bg-elements")}
+            alt="road"
+         />
+
+         {/* Верхняя дорога */}
+         {/* <img
+            src="img/pages/home/top-road.svg"
+            className={cn("road", "_top")}
+            alt="road"
+         /> */}
+
+         {/* Нижняя дорога */}
+         {/* <img
+            src="img/pages/home/bottom-road.svg"
+            className={cn("road", "_bottom")}
+            alt="road"
+         /> */}
       </div>
    );
 };
--- src/pages/Home/modules/Menu.tsx
+++ src/pages/Home/modules/Menu.tsx
@@ -1,5 +1,43 @@
+import classNames from "classnames/bind";
+import styles from "../Home.module.scss";
+const cn = classNames.bind(styles);
+
 const Menu = () => {
-   return <div>Menu</div>;
+   return (
+      <div className={cn("menu")}>
+         {/* Border */}
+         <img
+            src="img/pages/home/menu/border.svg"
+            className={cn("menu__border")}
+            alt=""
+         />
+
+         {/* Menu elements */}
+         <div className={cn("menu__body")}>
+            <ul className={cn("menu__list")}>
+               <li>
+                  <img src="img/pages/home/menu/Earn.svg" alt="Earn" />
+                  EARN
+               </li>
+               <li>
+                  <img src="img/pages/home/menu/Boost.svg" alt="Earn" />
+                  BOOST
+               </li>
+               <li>
+                  <img src="img/pages/home/menu/Farm.svg" alt="Earn" />
+               </li>
+               <li>
+                  <img src="img/pages/home/menu/Top.svg" alt="Earn" />
+                  TOP
+               </li>
+               <li>
+                  <img src="img/pages/home/menu/Stats.svg" alt="Earn" />
+                  STATS
+               </li>
+            </ul>
+         </div>
+      </div>
+   );
 };
 
 export default Menu;
