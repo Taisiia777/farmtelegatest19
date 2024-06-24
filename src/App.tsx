490df24ec9a219fbd072b57cb1832ffb7decfa67
EvgeniiSim <evgenijsimanin74@gmail.com>
2024-06-24 23:42:33 +0500

    Added scrollhack

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import {
    createBrowserRouter,
    createRoutesFromElements,
 } from "react-router-dom";
+import { useEffect } from "react";
 
 import { tg } from "./constants/app";
-import Invite from "./pages/Invite/Invite";
+import { Routes } from "./routes/routes";
 
 import Home from "./pages/Home/Home";
 import Stats from "./pages/Stats/Stats";
 import People from "./pages/People/People";
-import { Routes } from "./routes/routes";
+import Invite from "./pages/Invite/Invite";
 
 const router = createBrowserRouter(
    createRoutesFromElements(
@@ -29,6 +30,49 @@ tg.ready();
 tg.enableClosingConfirmation();
 
 const App = () => {
+   useEffect(() => {
+      const overflow = 100;
+      document.body.style.overflowY = "hidden";
+      document.body.style.marginTop = `${overflow}px`;
+      document.body.style.height = window.innerHeight + overflow + "px";
+      document.body.style.paddingBottom = `${overflow}px`;
+      document.body.style.minHeight = "100vh";
+
+      document.documentElement.style.overflow = "hidden";
+      document.documentElement.style.height = "auto";
+
+      window.scrollTo(0, overflow);
+
+      let ts: number | undefined;
+      const onTouchStart = (e: TouchEvent) => {
+         ts = e.touches[0].clientY;
+      };
+
+      const scrollableEl = document.getElementById("app");
+      if (scrollableEl) {
+         scrollableEl.style.overflowY = "auto";
+         scrollableEl.style.height = "100vh";
+      }
+
+      const onTouchMove = (e: TouchEvent) => {
+         if (scrollableEl) {
+            const scroll = scrollableEl.scrollTop;
+            const te = e.changedTouches[0].clientY;
+            if (scroll <= 0 && ts! < te) {
+               e.preventDefault();
+            }
+         } else {
+            e.preventDefault();
+         }
+      };
+      document.documentElement.addEventListener("touchstart", onTouchStart, {
+         passive: false,
+      });
+      document.documentElement.addEventListener("touchmove", onTouchMove, {
+         passive: false,
+      });
+   }, []);
+
    return <RouterProvider router={router} />;
 };
 
