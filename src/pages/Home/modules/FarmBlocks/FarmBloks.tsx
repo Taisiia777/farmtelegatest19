6392f1d9053e99fb95a572e8f67d4821eadadbcd
Taisiia777 <sporyshev.savelii@gmail.com>
2024-07-08 20:30:23 +0300

    faat: grass2

--- src/pages/Home/modules/FarmBlocks/FarmBloks.tsx
+++ src/pages/Home/modules/FarmBlocks/FarmBloks.tsx
@@ -815,214 +815,8 @@
 
 
 
-// import React, { useEffect, useState, useCallback } from "react";
-// import { useDispatch } from "react-redux";
-// import styles from "./FarmBlocks.module.scss";
-// import classNames from "classnames/bind";
-// import {
-//   selectEarthBlock,
-//   changeGrowthStage,
-//   pickWheat,
-//   calculateGrassEarnings,
-// } from "../../../../store/reducers/growthStages";
-// import { useAppSelector } from "../../../../store";
-// import useWheatTrunctaion from "../../hooks/useWheatTrunctation";
-// import { RootState } from "../../../../store";
-// import { setUser } from "../../../../store/reducers/userSlice";
-// import { updateGrassEarnings } from "../../../../store/reducers/userSlice";
-// import axios from "axios";
-
-// const cn = classNames.bind(styles);
-
-// type TLiga = "Wooden" | "Silver" | "Gold" | "Fire" | "Diamond"; // Определение типа TLiga
-
-// interface FarmBlocksProps {
-//   league: TLiga;
-// }
-
-// const FarmBloks: React.FC<FarmBlocksProps> = ({ league }) => {
-//   const dispatch = useDispatch();
-//   const user = useAppSelector((state: RootState) => state.user.user);
-//   const blocks = useAppSelector((state: RootState) => state.growthStages.blocks);
-
-//   useWheatTrunctaion();
-
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       for (let i = 1; i <= 9; i++) {
-//         dispatch(changeGrowthStage({ id: i }));
-//       }
-//       if (user) {
-//         const grassEarnings = calculateGrassEarnings(blocks, user.coinsPerHour);
-//         dispatch(updateGrassEarnings(grassEarnings));
-//         console.log("Новое значение прибыли: ", grassEarnings);
-//       }
-//     }, 10000);
-
-//     return () => clearInterval(interval);
-//   }, [dispatch, user, blocks]);
-
-//   return (
-//     <div className={cn("farmBlockWrap")}>
-//       <FarmBlock zIndex={9} id={1} league={league} />
-//       <FarmBlock zIndex={9} id={2} league={league} />
-//       <FarmBlock zIndex={9} id={3} league={league} />
-//       <FarmBlock zIndex={8} id={4} league={league} />
-//       <FarmBlock zIndex={7} id={5} league={league} />
-//       <FarmBlock zIndex={6} id={6} league={league} />
-//       <FarmBlock zIndex={6} id={7} league={league} />
-//       <FarmBlock zIndex={5} id={8} league={league} />
-//       <FarmBlock zIndex={4} id={9} league={league} />
-
-//       {/* Тень */}
-//       <img
-//         src="img/pages/home/earth-blocks-bg.svg"
-//         className={cn("farmBlock__shadow")}
-//         alt=""
-//       />
-//     </div>
-//   );
-// };
-
-// export default FarmBloks;
-
-// interface IFarmBlockProps {
-//   zIndex: number;
-//   id: number;
-//   league: TLiga;
-// }
-
-// const FarmBlock: React.FC<IFarmBlockProps> = ({ zIndex, id, league }) => {
-//   const farmBlock = useAppSelector((state) => selectEarthBlock(state, id));
-//   const user = useAppSelector((state: RootState) => state.user.user);
-//   const dispatch = useDispatch();
-//   const [localCoins, setLocalCoins] = useState(user ? user.coins : 0);
-//   const [touchedBlocks, setTouchedBlocks] = useState<Set<number>>(new Set());
-//   const [harvested, setHarvested] = useState(false);
-
-//   if (!farmBlock) return null;
-
-//   const handlePickWheat = async (blockId: number) => {
-//     if (farmBlock.stage !== "first") {
-//       let rewardMultiplier = 0;
-
-//       switch (farmBlock.stage) {
-//         case "second":
-//           rewardMultiplier = 1;
-//           break;
-//         case "third":
-//           rewardMultiplier = 2;
-//           break;
-//         case "fourth":
-//           rewardMultiplier = 3;
-//           break;
-//         default:
-//           return; // Ничего не делать, если стадия "first"
-//       }
-
-//       const reward = user ? user.coinsPerHour * rewardMultiplier : 0;
-
-//       if (user) {
-//         try {
-//           const response = await axios.patch(
-//             `https://coinfarm.club/user/${user.id}/earn/${reward}`
-//           );
-//           const updatedUser = response.data;
-
-//           // Обновление состояния пользователя и локальных монет
-//           dispatch(
-//             setUser({
-//               ...updatedUser,
-//               coins: parseFloat(updatedUser.coins),
-//               totalEarnings: parseFloat(updatedUser.totalEarnings),
-//             })
-//           );
-
-//           setLocalCoins(parseFloat(updatedUser.coins));
-//           console.log(localCoins);
-//         } catch (error) {
-//           console.error("Error:", error);
-//         }
-//       }
-//       dispatch(pickWheat({ id: blockId }));
-//       setHarvested(true);
-//       setTimeout(() => setHarvested(false), 400); // Сброс анимации после ее длительности
-//     }
-//   };
-
-//   const handleInteraction = useCallback(
-//     (e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>) => {
-//       e.preventDefault(); // Предотвращение стандартного поведения для предотвращения нежелательной прокрутки
-
-//       // Определение типа события
-//       const eventType = e.type;
-
-//       let target: HTMLElement | null = null;
-//       if (eventType === "mousemove" || eventType === "touchmove") {
-//         target = document.elementFromPoint(
-//           (e as React.MouseEvent<HTMLDivElement>).clientX ||
-//             (e as React.TouchEvent<HTMLDivElement>).touches[0].clientX,
-//           (e as React.MouseEvent<HTMLDivElement>).clientY ||
-//             (e as React.TouchEvent<HTMLDivElement>).touches[0].clientY
-//         ) as HTMLElement;
-//       }
-
-//       if (target) {
-//         const blockId = target.getAttribute("data-id");
-//         if (blockId) {
-//           const id = parseInt(blockId, 10);
-//           if (!touchedBlocks.has(id)) {
-//             setTouchedBlocks((prev) => new Set(prev).add(id));
-//             handlePickWheat(id);
-//           }
-//         }
-//       }
-//     },
-//     [handlePickWheat, touchedBlocks]
-//   );
-
-//   const handleTouchEnd = useCallback(() => {
-//     setTouchedBlocks(new Set()); // Сброс касаний при окончании взаимодействия
-//   }, []);
-
-//   return (
-//     <div
-//       className={cn("farmBlock")}
-//       style={{ zIndex }}
-//       data-id={id}
-//       data-stage={farmBlock.stage}
-//       onMouseMove={handleInteraction}
-//       onTouchMove={handleInteraction}
-//       onTouchEnd={handleTouchEnd}
-//     >
-//       <img
-//         src={`img/leagueStages/${league}.png`}
-//         className={cn("farmBlock__earth")}
-//         alt={league}
-//       />
-//       <img
-//         src={`img/growthStages/${farmBlock.stage}.png`}
-//         className={cn("farmBlock__growthStage", `_${farmBlock.stage}`)}
-//         alt="growth stage"
-//         data-id={id}
-//         data-stage={farmBlock.stage}
-//       />
-//       <img
-//         src="img/pages/home/money.svg"
-//         className={cn("farmBlock__money", { _anim: harvested })}
-//       />
-//     </div>
-//   );
-// };
-
-
-
-
-
-
 import React, { useEffect, useState, useCallback } from "react";
 import { useDispatch } from "react-redux";
-import { AppDispatch } from '../../../../store';
 import styles from "./FarmBlocks.module.scss";
 import classNames from "classnames/bind";
 import {
@@ -1034,35 +828,26 @@ import {
 import { useAppSelector } from "../../../../store";
 import useWheatTrunctaion from "../../hooks/useWheatTrunctation";
 import { RootState } from "../../../../store";
-import {
-  setUser,
-  updateGrassStage,
-  fetchUser,
-  updateGrassEarnings,
-  selectUser,
-} from "../../../../store/reducers/userSlice";
+import { setUser } from "../../../../store/reducers/userSlice";
+import { updateGrassEarnings } from "../../../../store/reducers/userSlice";
 import axios from "axios";
 
 const cn = classNames.bind(styles);
 
-type TLiga = "Wooden" | "Silver" | "Gold" | "Fire" | "Diamond";
+type TLiga = "Wooden" | "Silver" | "Gold" | "Fire" | "Diamond"; // Определение типа TLiga
 
 interface FarmBlocksProps {
   league: TLiga;
 }
 
-const FarmBlocks: React.FC<FarmBlocksProps> = ({ league }) => {
-  const dispatch = useDispatch<AppDispatch>();
-  const user = useAppSelector(selectUser);
+const FarmBloks: React.FC<FarmBlocksProps> = ({ league }) => {
+  const dispatch = useDispatch();
+  const user = useAppSelector((state: RootState) => state.user.user);
   const blocks = useAppSelector((state: RootState) => state.growthStages.blocks);
 
   useWheatTrunctaion();
 
   useEffect(() => {
-    if (user) {
-      dispatch(fetchUser(user.id));
-    }
-
     const interval = setInterval(() => {
       for (let i = 1; i <= 9; i++) {
         dispatch(changeGrowthStage({ id: i }));
@@ -1077,19 +862,17 @@ const FarmBlocks: React.FC<FarmBlocksProps> = ({ league }) => {
     return () => clearInterval(interval);
   }, [dispatch, user, blocks]);
 
-  useEffect(() => {
-    if (user) {
-      for (let i = 1; i <= 9; i++) {
-        dispatch(updateGrassStage({ userId: user.id, stage: user.grassGrowthStage || 0 }));
-      }
-    }
-  }, [dispatch, user]);
-
   return (
     <div className={cn("farmBlockWrap")}>
-      {Array.from({ length: 9 }, (_, i) => (
-        <FarmBlock key={i + 1} zIndex={9 - i} id={i + 1} league={league} />
-      ))}
+      <FarmBlock zIndex={9} id={1} league={league} />
+      <FarmBlock zIndex={9} id={2} league={league} />
+      <FarmBlock zIndex={9} id={3} league={league} />
+      <FarmBlock zIndex={8} id={4} league={league} />
+      <FarmBlock zIndex={7} id={5} league={league} />
+      <FarmBlock zIndex={6} id={6} league={league} />
+      <FarmBlock zIndex={6} id={7} league={league} />
+      <FarmBlock zIndex={5} id={8} league={league} />
+      <FarmBlock zIndex={4} id={9} league={league} />
 
       {/* Тень */}
       <img
@@ -1101,7 +884,7 @@ const FarmBlocks: React.FC<FarmBlocksProps> = ({ league }) => {
   );
 };
 
-export default FarmBlocks;
+export default FarmBloks;
 
 interface IFarmBlockProps {
   zIndex: number;
@@ -1116,7 +899,7 @@ const FarmBlock: React.FC<IFarmBlockProps> = ({ zIndex, id, league }) => {
   const [localCoins, setLocalCoins] = useState(user ? user.coins : 0);
   const [touchedBlocks, setTouchedBlocks] = useState<Set<number>>(new Set());
   const [harvested, setHarvested] = useState(false);
-  console.log(localCoins)
+
   if (!farmBlock) return null;
 
   const handlePickWheat = async (blockId: number) => {
@@ -1156,6 +939,7 @@ const FarmBlock: React.FC<IFarmBlockProps> = ({ zIndex, id, league }) => {
           );
 
           setLocalCoins(parseFloat(updatedUser.coins));
+          console.log(localCoins);
         } catch (error) {
           console.error("Error:", error);
         }
@@ -1168,8 +952,9 @@ const FarmBlock: React.FC<IFarmBlockProps> = ({ zIndex, id, league }) => {
 
   const handleInteraction = useCallback(
     (e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>) => {
-      e.preventDefault();
+      e.preventDefault(); // Предотвращение стандартного поведения для предотвращения нежелательной прокрутки
 
+      // Определение типа события
       const eventType = e.type;
 
       let target: HTMLElement | null = null;
@@ -1197,7 +982,7 @@ const FarmBlock: React.FC<IFarmBlockProps> = ({ zIndex, id, league }) => {
   );
 
   const handleTouchEnd = useCallback(() => {
-    setTouchedBlocks(new Set());
+    setTouchedBlocks(new Set()); // Сброс касаний при окончании взаимодействия
   }, []);
 
   return (
@@ -1229,3 +1014,7 @@ const FarmBlock: React.FC<IFarmBlockProps> = ({ zIndex, id, league }) => {
     </div>
   );
 };
+
+
+
+
--- src/store/reducers/userSlice.ts
+++ src/store/reducers/userSlice.ts
@@ -42,57 +42,7 @@
 
 
 
-// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-// export interface UserState {
-//     id: number;
-//     username: string;
-//     coins: number;
-//     incomeMultiplier: number;
-//     coinsPerHour: number;
-//     xp: number;
-//     level: number;
-//     totalEarnings?: number;
-//     grassEarnings?: number; // Добавляем новое свойство
-// }
-
-// const initialState: { user: UserState | null } = {
-//     user: null,
-// };
-
-// const userSlice = createSlice({
-//     name: 'user',
-//     initialState,
-//     reducers: {
-//         setUser(state, action: PayloadAction<UserState>) {
-//             state.user = action.payload;
-//         },
-//         clearUser(state) {
-//             state.user = null;
-//         },
-//         updateGrassEarnings(state, action: PayloadAction<number>) {
-//             if (state.user) {
-//                 state.user.grassEarnings = (state.user.grassEarnings || 0) + action.payload;
-//             }
-//         },
-//     },
-// });
-
-// export const { setUser, clearUser, updateGrassEarnings } = userSlice.actions;
-
-// export default userSlice.reducer;
-
-
-
-
-
-
-
-
-
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import axios from "axios";
-import { RootState, AppDispatch } from "../";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface UserState {
     id: number;
@@ -103,8 +53,7 @@ export interface UserState {
     xp: number;
     level: number;
     totalEarnings?: number;
-    grassEarnings?: number;
-    grassGrowthStage?: number;
+    grassEarnings?: number; // Добавляем новое свойство
 }
 
 const initialState: { user: UserState | null } = {
@@ -112,7 +61,7 @@ const initialState: { user: UserState | null } = {
 };
 
 const userSlice = createSlice({
-    name: "user",
+    name: 'user',
     initialState,
     reducers: {
         setUser(state, action: PayloadAction<UserState>) {
@@ -126,27 +75,16 @@ const userSlice = createSlice({
                 state.user.grassEarnings = (state.user.grassEarnings || 0) + action.payload;
             }
         },
-        setGrassGrowthStage(state, action: PayloadAction<number>) {
-            if (state.user) {
-                state.user.grassGrowthStage = action.payload;
-            }
-        },
     },
 });
 
-export const { setUser, clearUser, updateGrassEarnings, setGrassGrowthStage } = userSlice.actions;
-
-export const fetchUser = (userId: number) => async (dispatch: AppDispatch) => {
-    const response = await axios.get(`https://coinfarm.club/user/${userId}`);
-    dispatch(setUser(response.data));
-};
-
-export const updateGrassStage = ({ userId, stage }: { userId: number, stage: number }) => async (dispatch: AppDispatch) => {
-    const response = await axios.patch(`https://coinfarm.club/user/${userId}/grass/${stage}`);
-    dispatch(setGrassGrowthStage(response.data.grassGrowthStage));
-};
-
-export const selectUser = (state: RootState) => state.user.user;
+export const { setUser, clearUser, updateGrassEarnings } = userSlice.actions;
 
 export default userSlice.reducer;
+
+
+
+
+
+
 
