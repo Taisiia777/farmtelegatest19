6082dd5e504b816da485bb671346507d5af26019
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-02 16:56:57 +0000

    refs55

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -134,7 +134,7 @@ const Preloader = () => {
       // Запустить таймер на 5 секунд, после чего скрыть заставку
       const timer = setTimeout(() => {
          dispatch(loadingToggle(false));
-      }, 5000);
+      }, 10000);
 
       // Очистить таймер при размонтировании компонента
       return () => clearTimeout(timer);
