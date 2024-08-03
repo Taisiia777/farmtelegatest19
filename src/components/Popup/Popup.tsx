28b34a00b721274bd07a1465bdb5f6307d456795
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-03 17:50:20 +0000

    lang31

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,7 +1,8 @@
 import classNames from "classnames/bind";
 import styles from "./Popup.module.scss";
 import { ForwardedRef, ReactNode, forwardRef } from "react";
-
+import i18n from '../../i18n';
+import { useEffect } from "react";
 const cn = classNames.bind(styles);
 
 interface IPopupProps {
@@ -17,6 +18,36 @@ const Popup = forwardRef(
       { isOpen, children, onClose, borderlabel, isBlueBg = false }: IPopupProps,
       ref: ForwardedRef<HTMLDivElement>
    ) => {
+      useEffect(() => {
+        const initData = window.Telegram.WebApp.initDataUnsafe;
+        const userLanguage = initData.user?.language_code || 'en'; // Получаем язык пользователя
+        
+        if (['en', 'ru', 'uk'].includes(userLanguage)) { // Добавьте другие поддерживаемые языки
+          i18n.changeLanguage(userLanguage);
+        } else {
+          i18n.changeLanguage('en'); // Язык по умолчанию, если язык пользователя не поддерживается
+        }
+        if (userLanguage !== 'en') {
+         // Применяем стили ко всем элементам
+         document.querySelectorAll('.textMenu').forEach(element => {
+           if (element instanceof HTMLElement) { // Проверяем, что элемент является HTMLElement
+             element.style.fontSize = '14px';
+             element.style.fontWeight = '700';
+           }
+         });
+         document.querySelectorAll('.textMenu1').forEach(element => {
+            if (element instanceof HTMLElement) { // Проверяем, что элемент является HTMLElement
+              element.style.fontSize = '13px';
+              element.style.fontWeight = '700';
+            }
+          });
+          const labelElement = document.querySelector(`.${cn("popup__label")}`);
+          if (labelElement instanceof HTMLElement) {
+             labelElement.style.fontSize = '14px';
+          }
+       }
+        
+      }, []);
       return (
          <div className={cn("popup", isOpen && "_open")} id="popup">
             <div className={cn("popup__body")} ref={ref}>
