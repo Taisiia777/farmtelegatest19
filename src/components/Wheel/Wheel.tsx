2599e96933f4ab0142088324a2f9edd7ab797857
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-15 12:10:33 +0000

    77

--- src/components/Wheel/Wheel.tsx
+++ src/components/Wheel/Wheel.tsx
@@ -369,7 +369,7 @@ const spin = () => {
             <img src="img/pages/home/menu/WheelCenter.png" style={{width: '280px', display:'flex', zIndex:'11', position:'absolute', top: '155px', transform: `rotate(${rotation}deg)`,transition: isSpinning ? "transform 5s cubic-bezier(0.25, 0.1, 0.25, 1)" : "none",transformOrigin: "center" }} alt="Wheel" />
             <img src="img/pages/home/menu/WheelBorder.png" style={{width: '389px', display:'flex',  zIndex:'10',  position:'absolute', top: '100px'}} alt="Wheel" />
 
-            <img src="img/global/spin.png" className={cn("greeting__next")} style={{width: '122px', height: '46px', position: 'absolute', top: '70vh', left:'0'}} alt="Spin" onClick={spin} />
+            <img src="img/global/spin.png" className={cn("greeting__next")} style={{width: '122px', height: '46px', position: 'absolute', top: '70vh',  left: '50%', transform: 'translateX(-50%)'}} alt="Spin" onClick={spin} />
             <p className={`${cn("content__text", "_first")}` + ' textInvite3'} style={{width: '122px', height: '20px', position: 'absolute', top: '70vh', zIndex:'11'}}>
           {t('spin')}
           </p>
