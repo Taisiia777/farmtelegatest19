8f6a421e36cb6c436a88b733ee71569064b921ee
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-15 14:51:04 +0000

    84

--- src/components/Wheel/Wheel.tsx
+++ src/components/Wheel/Wheel.tsx
@@ -370,10 +370,10 @@ const spin = () => {
          //     transformOrigin: "center", // Центр вращения
          //   }}
             
-          > <p className={`${cn("content__text", "_first")}` + ' textWheel'} style={{width: '106px', height: '20px', position: 'absolute', top: '-27px', left: '14px', zIndex:'11'}}>
+          > <p className={`${cn("content__text", "_first")}` + ' textWheel'} style={{width: '95px', height: '20px', position: 'absolute', top: '-27px', left: '18px', zIndex:'11'}}>
             {spins} {t('spins')}
           </p>
-          <p className={`${cn("content__text", "_first")}` + ' textWheel1'} onClick={() => navigate(Routes.INVITE)} style={{width: '180px', height: '20px', position: 'absolute', top: '-27px', left: '130px', zIndex:'11'}}>
+          <p className={`${cn("content__text", "_first")}` + ' textWheel1'} onClick={() => navigate(Routes.INVITE)} style={{width: '165px', height: '20px', position: 'absolute', top: '-27px', left: '140px', zIndex:'11'}}>
           {t('more_spins')}
           </p>
             <img src="img/pages/home/menu/YourSpins.png" className={cn("greeting__next")} style={{width: '106px', height: '47px', position: 'absolute', top: '20px', left: '14px', zIndex:'10'}} alt="Spin"  />
