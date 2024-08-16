9f03f33822e1ce02d3accea2f86198a1ab99482d
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-16 16:02:30 +0000

    147

--- src/components/Wheel/Wheel.tsx
+++ src/components/Wheel/Wheel.tsx
@@ -36,15 +36,25 @@ const Wheel = () => {
    const [reward, setReward] = useState(0);
 
 
+// const sectors = [
+//   { name: "Sector 1", weight: 25, reward: 1000 },
+//   { name: "Sector 2", weight: 20, reward: 5000 },
+//   { name: "Sector 3", weight: 10, reward: 10000 },
+//   { name: "Sector 4", weight: 5, reward: 25000 },
+//   { name: "Sector 5", weight: 4.9, reward: 100000 },
+//   { name: "Sector 6", weight: 0.1, reward: 500000 },
+//   { name: "Sector 7", weight: 20, reward: 0 },
+//   { name: "Sector 8", weight: 15, reward: 0 }, // "Еще одно вращение"
+// ];
 const sectors = [
-  { name: "Sector 1", weight: 25, reward: 1000 },
-  { name: "Sector 2", weight: 20, reward: 5000 },
-  { name: "Sector 3", weight: 10, reward: 10000 },
-  { name: "Sector 4", weight: 5, reward: 25000 },
-  { name: "Sector 5", weight: 4.9, reward: 100000 },
-  { name: "Sector 6", weight: 0.1, reward: 500000 },
-  { name: "Sector 7", weight: 20, reward: 0 },
-  { name: "Sector 8", weight: 15, reward: 0 }, // "Еще одно вращение"
+  { name: "Sector 1", weight: 12.5, reward: 1000 },
+  { name: "Sector 2", weight: 12.5, reward: 5000 },
+  { name: "Sector 3", weight: 12.5, reward: 10000 },
+  { name: "Sector 4", weight: 12.5, reward: 25000 },
+  { name: "Sector 5", weight: 12.5, reward: 100000 },
+  { name: "Sector 6", weight: 12.5, reward: 500000 },
+  { name: "Sector 7", weight: 12.5, reward: 0 },
+  { name: "Sector 8", weight: 12.5, reward: 0 }, // "Еще одно вращение"
 ];
 
 useEffect(() => {
