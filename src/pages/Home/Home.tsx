6d1c6625f8ddfc75591bcd81f248b93149726bb5
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-19 15:33:13 +0000

    leve124

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -834,7 +834,7 @@ const Home = () => {
     useEffect(() => {
       // Здесь вызывается функция для ререндеринга монет
       renderCoins();
-    }, [coinState]);
+    }, [user]);
     
     const updateCoins = async (amount: number) => {
       if (user) {
