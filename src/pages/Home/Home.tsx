59db5641a96b9ef36b949bbf47e9e414b99dfc5b
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-19 01:00:35 +0000

    leve52

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,7 +15,7 @@ import classNames from "classnames/bind";
 import useWindowSize from "../../hooks/useWindowSize";
 import { setUserCoins1 } from '../../store/reducers/userCoinsSlice';
 import RainAnimation from './modules/RainAnimation';
-// import QRCodeComponent from './QRCodeComponent';
+import QRCodeComponent from './QRCodeComponent';
 import { openGuide } from "../../store/reducers/guide";
 import { useOutletContext } from 'react-router-dom';
 // import useWheatTrunctaion from "./hooks/useWheatTrunctation";
@@ -171,7 +171,7 @@ const Home = () => {
    const [tasks, setTasks] = useState<Task[]>([]);
    const [rainInterval, setRainInterval] = useState(0);
    const lastUpdateRef = useRef(Date.now());
-  //  const [showQRCode, setShowQRCode] = useState(false);
+   const [showQRCode, setShowQRCode] = useState(false);
    const [showGuide, setShowGuide] = useState(false);
    const { friends } = useOutletContext<OutletContext>();
 
@@ -298,15 +298,15 @@ const Home = () => {
       }, 500);
    }
 
-  //  useEffect(() => {
-  //   const checkIfDesktop = () => {
-  //     const userAgent = navigator.userAgent;
-  //     const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-  //     setShowQRCode(!isMobile);
-  //   };
+   useEffect(() => {
+    const checkIfDesktop = () => {
+      const userAgent = navigator.userAgent;
+      const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+      setShowQRCode(!isMobile);
+    };
   
-  //   checkIfDesktop();
-  // }, []);
+    checkIfDesktop();
+  }, []);
   
 
   //  const fetchUserReferralsCount = async (userId: number) => {
@@ -1182,8 +1182,8 @@ const Home = () => {
 
    return (
       <>
-       {/* <QRCodeComponent /> */}
-       {!false && (
+       <QRCodeComponent />
+       {!showQRCode && (
  <>
 
  {/* { isRain && <Clouds
