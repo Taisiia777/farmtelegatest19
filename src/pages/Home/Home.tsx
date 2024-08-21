ef67cca4f537a04b0537722c4bf4ca1f6db6e2f0
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-21 12:54:30 +0000

    13

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1698,9 +1698,9 @@ const renderCoins = () => {
         buttonText={t('play')}
       />
       <img style={{position: "absolute", top: "100%", left: "0", width: "100%"}} src="img/pages/home/menu/coming.png" />
-      <img style={{position: "absolute", top: "100%", left: "0", width: "100%"}} src="img/pages/home/menu/coming1.png" />
-      <img style={{position: "absolute", top: "100%", left: "0", width: "100%"}} src="img/pages/home/menu/coming2.png" />
-      <img style={{position: "absolute", top: "100%", left: "0", width: "100%"}} src="img/pages/home/menu/coming3.png" />
+      <img style={{position: "absolute", top: "200%", left: "0", width: "100%"}} src="img/pages/home/menu/coming1.png" />
+      <img style={{position: "absolute", top: "300%", left: "0", width: "100%"}} src="img/pages/home/menu/coming2.png" />
+      <img style={{position: "absolute", top: "400%", left: "0", width: "100%"}} src="img/pages/home/menu/coming3.png" />
 
       </>
 
