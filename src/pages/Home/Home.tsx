b9f2f5f73bb3c1fed4b18f3bcce92a3bfa075a8a
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-27 17:12:26 +0000

    35

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1328,8 +1328,7 @@ console.log(response1)
         const latestRainReward = rainRewards[rainRewards.length - 1];
         const rewardTime = new Date(latestRainReward.description);
         const currentTime = new Date();
-        // const timeDifference = (currentTime.getTime() - rewardTime.getTime()) / (1000 * 60 * 60); // in hours
-        const timeDifference = (currentTime.getTime() - rewardTime.getTime()) / (1000); // in hours
+        const timeDifference = (currentTime.getTime() - rewardTime.getTime()) / (1000 * 60 * 60); // in hours
 
         // Устанавливаем прогресс до следующего дождя
         setCurrentRainProgress(timeDifference);
