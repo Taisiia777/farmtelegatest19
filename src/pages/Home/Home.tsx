54e294f723ac2af3798cf0cd256a3fa19d2881e5
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-29 18:40:04 +0000

    24

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -398,7 +398,7 @@ const Home = () => {
     });
     document.querySelectorAll('.textMenu1').forEach(element => {
        if (element instanceof HTMLElement) { // Проверяем, что элемент является HTMLElement
-         element.style.fontSize = '13px';
+         element.style.fontSize = '12px';
          element.style.fontWeight = '700';
        }
      });
