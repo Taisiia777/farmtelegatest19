86b9bf094b202889e6b01ce032a755242def5b79
Taisiia777 <sporyshev.savelii@gmail.com>
2024-09-05 09:18:23 +0000

    19

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1256,10 +1256,10 @@ console.log(response1)
       try {
         await axios.patch(`https://coinfarm.club/api/user/${user?.id}/xp/${amount}`);
               // Отправляем запрос на сервер для обновления множителя
-      const response1 = await axios.put(`https://coinfarm.club/api/user/${user.id}`, {
-        coins: user.coins
-      });
-      console.log(response1)
+      // const response1 = await axios.put(`https://coinfarm.club/api/user/${user.id}`, {
+      //   coins: user.coins
+      // });
+      // console.log(response1)
       } catch (error) {
         console.error('Error updating XP:', error);
       }
