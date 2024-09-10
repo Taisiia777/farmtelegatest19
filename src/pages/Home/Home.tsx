537e01ac85bbf9fe7337a72748be762b1c82ab8a
Taisiia777 <sporyshev.savelii@gmail.com>
2024-09-10 14:17:40 +0000

    5

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1231,13 +1231,13 @@ console.log(response1)
         const updatedUserData = response.data;
     
         // Сохраняем актуальные данные в Redux после синхронизации с сервером
-        dispatch(setUser(updatedUserData));
-        // dispatch(
-        //   setUser({
-        //     ...user, // Берем текущее состояние пользователя
-        //     xp: updatedUserData.xp, // Обновляем только xp
-        //   })
-        // );
+        // dispatch(setUser(updatedUserData));
+        dispatch(
+          setUser({
+            ...user, // Берем текущее состояние пользователя
+            xp: updatedUserData.xp, // Обновляем только xp
+          })
+        );
         console.log("Синхронизация заработка с сервером прошла успешно:", updatedUserData);
       } catch (error) {
         console.error("Ошибка при синхронизации displayEarnings:", error);
