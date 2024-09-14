7b4594ba45ee5d1260ad48be38253d165fae57c3
Taisiia777 <sporyshev.savelii@gmail.com>
2024-09-14 23:12:01 +0000

    19

--- src/components/GamesBlock/GamesBlock1.tsx
+++ src/components/GamesBlock/GamesBlock1.tsx
@@ -25,7 +25,7 @@ const GamesBlock = ({
     dispatch(openCombo());
  };
    return (
-      <div className={cn("block")} style={{position: "absolute", top: "150%", left: "0", width: "100%",
+      <div className={cn("block")} style={{position: "absolute", top: "100%", left: "0", width: "100%", height: "126px",
          backgroundImage: `url("img/global/border-block/item.svg")`,
          backgroundSize: "cover",
          backgroundPosition: "center"}}>
