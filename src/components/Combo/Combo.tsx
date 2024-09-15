b18c4b978bd1f31dfcc2db65cc253ebc064449f6
Taisiia777 <sporyshev.savelii@gmail.com>
2024-09-15 19:06:22 +0000

    34

--- src/components/Combo/Combo.tsx
+++ src/components/Combo/Combo.tsx
@@ -390,17 +390,17 @@ useEffect(() => {
     id="fortune1"
     style={{
       zIndex: 13,
-      width: "90vw",
+      width: "400px",
       height: "240px",
       position: "absolute",  // Абсолютное позиционирование
-      top: "40%",            // Отступ сверху 50%
+      top: "50%",            // Отступ сверху 50%
       left: "50%",           // Отступ слева 50%
       transform: "translate(-50%, -50%)",  // Смещаем по оси X и Y для точного центрирования
       textAlign: "center"    // Центрирование текста внутри блока
     }}
   >
     <img src="img/global/popup-border.png" className={cn("greeting__border")} alt="border" />
-    <strong className={`${cn("greeting__label", "_first")}` + ' textInvite3'}>Таймер</strong>
+    <strong className={`${cn("greeting__label", "_first")}` + ' textInvite3'} style={{top: "30px"}}>Таймер</strong>
     <div className={cn("greeting__content", "content")}       style={{ display: "flex", justifyContent: "center", gap: "15px", marginTop: "100px" }}  // Выравнивание по центру и промежутки
     >
     <div style={{ textAlign: "center" }}>
