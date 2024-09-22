12e7e65b0e28dfcd23f34800723ae02e85c16972
Taisiia777 <sporyshev.savelii@gmail.com>
2024-09-22 10:05:30 +0000

    4

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1942,7 +1942,7 @@ const fertFormattedPrice = parseFloat(fertilizersState.info.price) >= 1000000000
                      setEarnPopupOpen(false);
                      setGamesPopupOpen(false);
                      setLeaguesopupOpen(false)
-                     window.scrollTo(0, 100);
+                    //  window.scrollTo(0, 100);
 
                   }}
                   className={cn("close")}
