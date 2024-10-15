74515657d7054de1b07f0c92aae0974368ef355d
Taisiia777 <sporyshev.savelii@gmail.com>
2024-10-15 19:01:17 +0000

    26

--- src/pages/Home/modules/Menu.tsx
+++ src/pages/Home/modules/Menu.tsx
@@ -118,7 +118,7 @@ const Menu = ({ onBoostOpen, onEarnOpen, onCoinsOpen, onGamesOpen}: IMenuProps) 
             <div className={cn("menu__body")}>
                <ul className={cn("menu__list")}>
                <li onClick={() => onBoostOpen()}>
-                     <img src="img/pages/home/menu/Boost.svg" style={{width:"46px", height: "46px"}} alt="Boost"/>
+                     <img src="img/pages/home/menu/Boost.svg" style={{width:"50px", height: "50px"}} alt="Boost"/>
                      <span className="textShadow textMenu1">{t('fertilizers')}</span>
                   </li>
                   <li  onClick={() => onCoinsOpen()}>
